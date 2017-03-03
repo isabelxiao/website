8e6256c868addc57baffee90406abe4a2c68b670
Isabel <isabel.shiao@gmail.com>
2017-03-02 23:36:22 -0500

    Assignment 3

---
+++ js/sketch.js
@@ -0,0 +1,147 @@
+// var myFirstVariable = 20;
+// var secondVariable = 35;
+// var myName = "Isabel";
+// var amIAmerican = false;
+// var numbers = [12, 214, 523, 645, 86];
+// var myObject = {name: "Isabel", age: "18"};
+//var circleSize = 50;
+
+// function preload(){ // load the data before the page is open
+// 	// run the preload function first, otherwise Javascript run all the function at the same time
+
+// }
+
+// var myData = [12, 43, 15, 25, 34];
+// var labels = ['Colombia', 'Peru', 'Venezuela', 'Chile', 'Argentina'];
+
+var refugeeTable; // building the variable outside the function, all of the function could access the variable
+var topRefugeesTable = new p5.Table;
+var maxTotal = 0;
+var maxLabel = 0;
+var maxLength = 550;
+
+
+function preload() { // preload runs first before all others
+	refugeeTable = loadTable('../data/RefugeesUNHCR.csv', 'csv', 'header'); // it's a csv, and has the header
+	console.log('Done loading table...');
+}
+
+// function setup(){ //
+// 	console.log(myFirstVariable); // console is a great way to troubleshooting
+// 	print(myFirstVariable);
+// 	var thirdVariable = myFirstVariable + secondVariable + myName; // concatenate treat the number as text, treat them in order
+// 	var forthVariable = myName + secondVariable + myFirstVariable;
+// 	print(thirdVariable);
+// 	print(forthVariable);
+// 	print(typeof(thirdVariable));
+// 	print(numbers[0]);
+// 	for (var i = 0; i < 100; i++) {
+// 		print(i);
+// 	}
+// 	var i = 0;
+// 	while (i < 100) {
+// 		print(i);
+// 		i++;
+// 	}
+// 	createCanvas(800, 800); // 500 pixol
+// 	console.log('All good... Proceed!!!');
+// }
+
+function setup() { // before the draw()
+	createCanvas(800, 4000);
+	// textAlign(LEFT, TOP); ?
+	textSize(12);
+	console.log('Setup complete');
+	print(refugeeTable.getRowCount() + ' rows loaded');
+	print(refugeeTable.getColumnCount() + ' columns loaded');
+	for (var i = 0; i < refugeeTable.getRowCount(); i++) {
+		maxTotal = max(refugeeTable.getNum(i, 'Total'), maxTotal);
+		maxLabel = max(refugeeTable.getString(i, 'Country').length, maxLabel);
+	}
+	print('Maximum total is ' + maxTotal);
+	print('Maximum label length is ' + maxLabel);
+	createNewTable();
+}
+
+function createNewTable() {
+	topRefugeesTable.addColumn('Country');
+	topRefugeesTable.addColumn('Total');
+	for (var i = 0; i < refugeeTable.getRowCount(); i++) {
+		var totalRefugees = refugeeTable.getNum(i, 'Total');
+		if (totalRefugees >= 100000) {
+			var newRow = topRefugeesTable.addRow();
+			newRow.setString('Country', refugeeTable.getString(i, 'Country'));
+			newRow.setNum('Total', refugeeTable.getNum(i, 'Total'));
+		}
+	}
+	print('New top refugee table created');
+}
+
+function drawCountries(tableName) {
+	fill(0);
+	noStroke();
+	textAlign(LEFT, TOP);
+	for (var i = 0; i < tableName.getRowCount(); i++) {
+		var total = tableName.getNum(i, 'Total');
+		var length = map(total, 0, maxTotal, 0, maxLength);
+		rect(maxLabel * 5, 10 + 15 * i, length, 12);
+		text(nfc(total, 0), maxLabel * 5 + length + 5, 10 + 15 * i);
+	}
+
+	textAlign(RIGHT, TOP);
+	for (var i = 0; i < tableName.getRowCount(); i++) {
+		text(tableName.getString(i, 'Country'), maxLabel * 5 - 5, 10 + 15 * i);
+	}
+}
+
+function draw() {
+	background(255);
+	drawCountries(topRefugeesTable);
+}
+
+// function draw() {
+// 	background(255);
+// 	fill(0);
+// 	noStroke();
+// 	textAlign(LEFT, TOP);
+// 	for (var i = 0; i < refugeeTable.getRowCount(); i++) {
+// 		var total = refugeeTable.getNum(i, 'Total');
+// 		var rectTotal = map(total, 0, maxTotal, 0, maxLength); // map functionis super useful
+// 		rect(maxLabel * 5, 50 + 20 * i, rectTotal, 15);
+// 		text(nfc(total, 0), maxLabel * 5 + rectTotal + 5, 50 + 20 * i);
+// 	}
+
+// 	textAlign(RIGHT, TOP);
+// 	for (var i = 0; i < refugeeTable.getRowCount(); i++) {
+// 		text(refugeeTable.getString(i, 'Country'), maxLabel * 5 - 5, 50 + 20 * i);
+// 	}
+// }
+
+// function draw() {
+// 	background(255);
+// 	textAlign(RIGHT, TOP);
+// 	for (var i = 0; i < myData.length; i++) {
+// 		rect(80, 50 + 20 * i, myData[i] * 10, 15); // rect(X position, Y position, width, height)
+// 		text(labels[i], 75, 50 + 20 * i); // text(text, X position, Y position)
+// 		text(myData[i], 95 + myData[i] * 10, 50 + 20 * i);
+// 	}
+
+// }
+
+
+// function draw(){ // running 60 times per second
+// 	background(200, 200, 200); // RGB: 0 black
+// 	if (mouseIsPressed) {
+// 		fill(255);
+// 	} else {
+// 		fill(0);
+// 	}
+// 	stroke(200, 0, 50);
+// 	strokeWeight(1);
+// 	ellipse(mouseX, mouseY, circleSize, circleSize); // x, y coordinate, width, height
+// }
+
+// function myFirstFunction(x1, x2, x3){
+// 	var x4 = x1 * x2 * x3;
+// 	return x4;
+// }
---
+++ js/sketch1.js
@@ -0,0 +1,74 @@
+var childTable;
+var gender = {"boys": 0, "girls": 0};
+var age = {"0 - 5": 0, "6 - 11": 0, "12 - 17": 0};
+var yearList = {1984: 0, 1990: 0, 1991: 0, 1994: 0, 1995: 0, 1996: 0, 1997: 0, 1998: 0, 1999: 0, 2000: 0, 2001: 0, 2002: 0, 2003: 0, 2004: 0, 2005: 0, 2006: 0, 2007: 0, 2008: 0, 2009: 0, 2010: 0, 2011: 0, 2012: 0, 2013: 0, 2014: 0, 2015: 0,};
+var cnv;
+
+
+function preload() {
+	childTable = loadTable('../data/children_sexual.csv', 'csv', 'header');
+	console.log("Done loading table");
+}
+
+function setup() {
+	createCanvas(1000, 2000);
+	textSize(28);
+	console.log("Setup complete");
+	print(childTable.getRowCount() + " rows loaded");
+	print(childTable.getColumnCount() + " colums loaded");
+
+	for (var i = 0; i < childTable.getRowCount(); i++) {
+		if (childTable.getString(i, 'GENERO') == "Mujer") {
+			gender["girls"] += childTable.getNum(i, 'TOTAL');
+		} else if (childTable.getString(i, 'GENERO') == "Hombre") {
+			gender["boys"] += childTable.getNum(i, 'TOTAL');
+		}
+
+		if (childTable.getString(i, 'CICLO VITAL') == "entre 0 y 5") {
+			age["0 - 5"] += childTable.getNum(i, 'TOTAL');
+		} else if (childTable.getString(i, 'CICLO VITAL') == "entre 6 y 11") {
+			age["6 - 11"] += childTable.getNum(i, 'TOTAL');
+		} else if (childTable.getString(i, 'CICLO VITAL') == "entre 12 y 17") {
+			age["12 - 17"] += childTable.getNum(i, 'TOTAL');
+		}
+		var aYear = childTable.getNum(i, 'ANIO OCURRENCIA');
+		yearList[aYear] += childTable.getNum(i, 'TOTAL');
+	}
+}
+
+function draw() {
+	fill(0);
+	noStroke();
+	// gender
+	textAlign(RIGHT, TOP);
+	text("Victims by gender", 650, 10);
+	rect(300, 50, gender["girls"], 30);
+	text("Girls", 290, 50);
+	rect(300, 50 + 40, gender["boys"], 30);
+	text("Boys", 290, 50 + 40);
+
+	textAlign(LEFT, TOP);
+	text(gender["girls"], 310 + gender["girls"], 50);
+	text(gender["boys"], 310 + gender["boys"], 50 + 40);
+
+	// age group
+	textAlign(RIGHT, TOP);
+	text("Victims by age groups", 675, 250);
+	rect(300, 300, age["0 - 5"], 30);
+	text("0 - 5 years old", 290, 300);
+	rect(300, 300 + 40, age["6 - 11"], 30);
+	text("6 - 11 years old", 290, 300 + 40);
+	rect(300, 300 + 40 * 2, age["12 - 17"], 30);
+	text("12 - 17 years old", 290, 300 + 40 * 2);
+
+	textAlign(LEFT, TOP);
+	text(age["0 - 5"], 310 + age["0 - 5"], 300);
+	text(age["6 - 11"], 310 + age["6 - 11"], 300 + 40);
+	text(age["12 - 17"], 310 + age["12 - 17"], 300 + 40 * 2);
+
+	// year
+	// for (var i = 0; i < Obejct.keys(yearList).length; i++) {
+
+	// }
+
+}
